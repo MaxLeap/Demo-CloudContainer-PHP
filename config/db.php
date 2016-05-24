2d5e55000f3845790f348865a304df5f7075703e
heyang <heyang@ilegend.local>
2016-05-24 12:10:02 +0800

    add php yii framework codeg

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=circev1.uat.maxleap.cn:3307/5742a9ea70c67600015bede4;dbname=5742a9ea70c67600015bede4',
+    'username' => '5742a9ea70c67600015bede4',
+    'password' => '5742a9ea70c67600015bede4',
+    'charset' => 'utf8',
+];
